d5e1f791fb7bb1c3297b2d0474fa261bbaeb74c7
Adiyanthy Velpandian <adiyanthy@gmail.com>
2022-05-22 10:48:46 +0530

    First commit

---
+++ ContainDulplicate.py
@@ -0,0 +1,7 @@
+ContainsDuplicate(self, nums: List[int]) -> bool:
+            nums.sort()
+            for i in range(0,len(nums)-1):
+                if nums[i] == nums[i+1]:
+                    return True
+                return False
+
